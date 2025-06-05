9181bbb0ae5b4a6af215a43db1ce0d1c7212aa99
Joaogalescky <joaocvgalescky@gmail.com>
2025-06-05 01:12:03 -0300

    feat: hsvToRgb e pino

--- HSV_JoyStick_Saturation.ino
+++ HSV_JoyStick_Saturation.ino
@@ -12,8 +12,45 @@ Hue e Value, considere o valor de Saturation 1.
 #define PIN_LED_G 10
 #define PIN_LED_B 9
 
+byte rgb[3];
+
 double JoyStick_X = A0
 double JoyStick_Y = A1
+
+  /**
+* Converts an HSV color value to
+* RGB.
+* Assumes h, s, and v are
+* contained in the set [0, 1] and
+* returns r, g, and b in the
+* set [0, 255].
+*
+* @param Number h The hue
+* @param Number s The saturation
+* @param Number v The value
+* @return Array The RGB
+* representation
+*/
+  void
+  hsvToRgb(double h, double s, double v, byte rgb[]) {
+  double r, g, b;
+  int i = int(h * 6);
+  double f = h * 6 - i;
+  double p = v * (1 - s);
+  double q = v * (1 - f * s);
+  double t = v * (1 - (1 - f) * s);
+  switch (i % 6) {
+    case 0: r = v, g = t, b = p; break;
+    case 1: r = q, g = v, b = p; break;
+    case 2: r = p, g = v, b = t; break;
+    case 3: r = p, g = q, b = v; break;
+    case 4: r = t, g = p, b = v; break;
+    case 5: r = v, g = p, b = q; break;
+  }
+  rgb[0] = r * 255;
+  rgb[1] = g * 255;
+  rgb[2] = b * 255;
+}
 
 void setup() {
 
