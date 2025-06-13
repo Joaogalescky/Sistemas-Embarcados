6b27f493c4bca8ece4ccb9011bbcc718c1b61a9f
Joaogalescky <joaocvgalescky@gmail.com>
2025-06-13 00:11:13 -0300

    feat: pinos

--- HSV_Detector.ino
+++ HSV_Detector.ino
@@ -1,7 +1,24 @@
+/*
+2) Identificar cores com o Arduino utilizando um LDR e um led RGB. O sistema deve
+ser calibrado para no mínimo 7 cores. O programa deve verificar o acionamento de um
+botão, caso acionado, deve identificar a cor do objeto e imprimir na saída serial os
+componentes RGB, também de configurar o led para que a cor seja a mais próxima da
+cor do material amostrado.
+*/
+
+//Autores: João Vitor Campõe Galescky & Gabriel Albuquerque
+
+//pinos
+const int PIN_LED_R = 11;
+const int PIN_LED_G = 10;
+const int PIN_LED_B = 9;
+const int PIN_LDR = A0;
+const int PIN_BUTTON = 2;
+
 void setup() {
     // put your setup code here, to run once:
-}
+};
 
 void loop() {
     // put your main code here, to run repeatedly:
-}
+};
