7ca7f8ffa4155658948395a291fedc8fe68398bd
Joaogalescky <joaocvgalescky@gmail.com>
2025-06-13 00:12:37 -0300

    feat: conversor HSV para RGB

--- HSV_Detector.ino
+++ HSV_Detector.ino
@@ -15,6 +15,40 @@ const int PIN_LED_B = 9;
 const int PIN_LDR = A0;
 const int PIN_BUTTON = 2;
 
+//variavel
+byte rgb[3];
+
+/**
+* Converts an HSV cor value to RGB.
+* Assumes h, s, and v are contained in the set [0, 1] and
+* returns r, g, and b in the set [0, 255].
+* @param Number h The hue
+* @param Number s The saturation
+* @param Number v The value
+* @return Array The RGB representation
+*/
+void hsvToRgb(double h, double s, double v, byte rgb[]) {
+  double r, g, b;
+  int i = int(h * 6);
+  double f = h * 6 - i;
+  double p = v * (1 - s);
+  double q = v * (1 - f * s);
+  double t = v * (1 - (1 - f) * s);
+
+  switch (i % 6) {
+    case 0: r = v, g = t, b = p; break;
+    case 1: r = q, g = v, b = p; break;
+    case 2: r = p, g = v, b = t; break;
+    case 3: r = p, g = q, b = v; break;
+    case 4: r = t, g = p, b = v; break;
+    case 5: r = v, g = p, b = q; break;
+  }
+
+  rgb[0] = r * 255;
+  rgb[1] = g * 255;
+  rgb[2] = b * 255;
+}
+
 void setup() {
     // put your setup code here, to run once:
 };
