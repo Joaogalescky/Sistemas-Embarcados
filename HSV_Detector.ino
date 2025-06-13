7b4d2ba83cec82115ce763a5a751a33da56b9eec
Joaogalescky <joaocvgalescky@gmail.com>
2025-06-13 00:24:19 -0300

    feat: checagem de botão para identificar cor no loop

--- HSV_Detector.ino
+++ HSV_Detector.ino
@@ -255,5 +255,9 @@ void setup() {
 };
 
 void loop() {
-    // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly:
+  if (checarBotaoPressionado()) {
+    identificarCor();
+  }
+  delay(50);
 };
