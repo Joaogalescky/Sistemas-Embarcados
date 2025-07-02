48a7a2ec7a2966ded02a79d87b25392c78a893cc
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2025-07-02 00:53:31 -0300

    fix: lógica de retrocesso e diferença
    Lógica de retrocesso no for e diferença entre início e fim para avanço em grau pelo tempo.

--- ServoParametros.ino
+++ ServoParametros.ino
@@ -1,37 +1,52 @@
+// João Galescky & Gabriel Albuquerque
+/* Questão
+Faça uma função para movimentar o servo, passando 3 parâmetros: posição inicial, posição
+final, tempo de deslocamento (considere a variação mínima de 1 grau).
+void moveServo(int posInicial, int posFinal, long tempoMillis){}
+*/
+
 #include <Servo.h>
 
 Servo myservo;
 
 const int PIN_SERV = 3;
-int tempoExecucao = 0;
-int diferenca;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   myservo.attach(PIN_SERV);
   myservo.write(0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  moveServo(10, 120, 5000)
+  moveServo(10, 120, 5000);
+  delay(2000);
+  moveServo(120, 10, 5000);
+  delay(2000);
 }
 
 void moveServo(int posInicial, int posFinal, long tempoMillis) {
-  unsigned long tempo_atual = millis();
+  int diferenca;
 
-  if (posFinal < posInicial) {
-    maior = posFinal;
-    posFinal = posInicial;
-    posInicial = maior;
+  // dá para usar => int diferenca = abs(posFinal - posInicial);
+  if (posInicial > posFinal) {
+    diferenca = posInicial - posFinal;
+  } else {
+    diferenca = posFinal - posInicial;
   }
-  diferenca = posFinal
 
-  tempoExecucao /= ;
+  int grauPorDelay = tempoMillis / diferenca;
 
-  for (int i = posInicial; i <= posFinal; i++){
-    myservo.write(i);
-    delay(tempoMillis);
+  // avançar, caso a = 10 e b = 50
+  if (posInicial < posFinal) {
+    for (int i = posInicial; i <= posFinal; i++){
+      myservo.write(i);
+      delay(grauPorDelay);
+    }
+  // retroceder, caso a = 50 e b = 10
+  } else {
+    for (int i = posInicial; i >= posFinal; i--){
+      myservo.write(i);
+      delay(grauPorDelay);
+    }
   }
 }
