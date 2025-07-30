d19e8f3e7d2973e5cd5c1ac021769f78d55e74e8
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2025-07-29 22:27:31 -0300

    feat: aula 13 - sensor de distância
    HC-SR04

---
+++ SensorDistancia.ino
@@ -0,0 +1,32 @@
+const int echoPin = 4;
+const int triggerPin = 3;
+long duration, cm, mm;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(echoPin, INPUT);
+  pinMode(triggerPin, OUTPUT);
+  digitalWrite(triggerPin, LOW);
+}
+
+void loop() {
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(5);
+
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+
+  cm = ((double)duration/2) / 29.387; // Aqui ele está convertendo para cm
+  // mm = ((double)duration/2) / 293.87; // Aqui ele está convertendo para mm
+
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+
+  delay(250);
+}
