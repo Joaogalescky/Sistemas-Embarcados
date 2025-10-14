3babdf9703f04bc0f17852c7f75d2f48c96c97d2
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2025-10-14 01:00:19 +0000

    feat: atv2 - fsm - complet

--- atv2_fsm_lista_5b/atv2_fsm_lista_5b.ino
+++ atv2_fsm_lista_5b/atv2_fsm_lista_5b.ino
@@ -7,58 +7,255 @@
   cíclica e a reversão de movimento.
 */
 
-/* Itens
-   - Botão único de controle (PIN_BTN)
-   - Dois sensores de fim de curso (PIN_SENSOR_OPEN, PIN_SENSOR_CLOSE)
-   - Motor controlado por PIN_MOTOR_EN (liga/desliga) e PIN_MOTOR_DIR (direção)
-   - LED_OPEN, LED_CLOSE (indicadores de direção)
-   - LED_ALERT (piscando enquanto há movimento)
-   - Timeout de segurança: 30s (TIMEOUT_MOVEMENT)
-   - Debounce para botões/sensores (50 ms)
+/* Pinos
+  - PIN_BTN: botão de controle (entrada com pullup)
+  - PIN_BTN_SENSOR_ABERTO: sensor "totalmente aberto" (entrada com pullup)
+  - PIN_BTN_SENSOR_FECHADO: sensor "totalmente fechado" (entrada com pullup)
+  - PIN_LED_ABERTO: led indicando que está abrindo
+  - PIN_LED_FECHADO: led indicando que está fechando
+  - PIN_LED_ALERTA: led de alerta que pisca quando em movimento
 */
 
 // Pinos
 #define PIN_BTN 2
-#define PIN_SENSOR_ABERTO 3   // Sensor
-#define PIN_SENSOR_FECHADO 4  // Sensor
-#define PIN_MOTOR_ABERTO 5    // Motor
-#define PIN_MOTOR_FECHADO 6   // Motor
-#define PIN_LED_ABERTO 8      // Led
-#define PIN_LED_FECHADO 9     // Led
-#define PIN_LED_ALERTA 10     // Led
+#define PIN_BTN_SENSOR_ABERTO 3
+#define PIN_BTN_SENSOR_FECHADO 4
+#define PIN_LED_ABERTO 10
+#define PIN_LED_FECHADO 9
+#define PIN_LED_ALERTA 8
 
-const long DEBOUNCE_MS = 50;
-const long TIMEOUT_MS = 30000UL  // ? Oque é UL
-  const long PISCAR_INTERVALO_MS = 500;
+// Variáveis
+const unsigned long DEBOUNCE = 50;
+const unsigned long TIMEOUT = 30000;
+const unsigned long TEMPO_ALERTA = 500;
 
-enum Estado {
-  FECHADO,
-  ABERTO,
-  FECHADO_MOVENDO,
-  ABERTO_MOVENDO,
-  PARADO
+unsigned long ultimoDebounce = 0;
+int ultimaLeituraBtn = HIGH;
+int ultimaEstadoBtn = HIGH;
+bool eventoBtn = false;
+
+unsigned long inicioMovimento = 0;
+unsigned long ultimoAlerta = 0;
+bool alertaLed = false;
+
+enum Estado
+{
+  EST_PARADO,
+  EST_ABRINDO,
+  EST_FECHANDO,
+  EST_INTERROMPIDO
 };
 
-Estado estado = PARADO;
+Estado estado = EST_PARADO;
+Estado estadoAnterior = estado;
 
-enum Direcao {
+enum Direcao
+{
   DIR_NENHUM,
   DIR_ABRINDO,
   DIR_FECHANDO
 };
 
-Direcao ultimaDirecao = DIR_NENHUM;
+Direcao direcao = DIR_NENHUM;
+Direcao direcaoAnterior = direcao;
 
-struct DebouncedEntrada {
-  int pin;
-  bool estadoAtual;
-  int 
+inline bool ehSensorAberto()
+{
+  return digitalRead(PIN_BTN_SENSOR_ABERTO) == LOW;
+}
+inline bool ehSensorFechado()
+{
+  return digitalRead(PIN_BTN_SENSOR_FECHADO) == LOW;
 }
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Iniciando Sistema de Controle de Portao Eletronico...");
+  // pinos de entrada
+  pinMode(PIN_BTN, INPUT_PULLUP);
+  pinMode(PIN_BTN_SENSOR_ABERTO, INPUT_PULLUP);
+  pinMode(PIN_BTN_SENSOR_FECHADO, INPUT_PULLUP);
+  // pinos de saída
+  pinMode(PIN_LED_ABERTO, OUTPUT);
+  pinMode(PIN_LED_FECHADO, OUTPUT);
+  pinMode(PIN_LED_ALERTA, OUTPUT);
+  digitalWrite(PIN_LED_ABERTO, LOW);
+  digitalWrite(PIN_LED_FECHADO, LOW);
+  digitalWrite(PIN_LED_ALERTA, LOW);
+  ultimaLeituraBtn = digitalRead(PIN_BTN);
+  ultimaEstadoBtn = ultimaLeituraBtn;
+  ultimoDebounce = millis();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void AbrirPortao()
+{
+  digitalWrite(PIN_LED_ABERTO, HIGH);
+  digitalWrite(PIN_LED_FECHADO, LOW);
+  inicioMovimento = millis();
+  Serial.println("Abrindo portao...");
+}
+
+void FecharPortao()
+{
+  digitalWrite(PIN_LED_ABERTO, LOW);
+  digitalWrite(PIN_LED_FECHADO, HIGH);
+  inicioMovimento = millis();
+  Serial.println("Fechando portao...");
+}
+
+void PararMotor()
+{
+  digitalWrite(PIN_LED_ABERTO, LOW);
+  digitalWrite(PIN_LED_FECHADO, LOW);
+  inicioMovimento = 0;
+  Serial.println("Parando portao...");
+}
+
+void Alerta()
+{
+  long tempoAtual = millis();
+  if (estado == EST_ABRINDO || estado == EST_FECHANDO)
+  {
+    if (tempoAtual - ultimoAlerta >= TEMPO_ALERTA)
+    {
+      ultimoAlerta = tempoAtual;
+      alertaLed = !alertaLed;
+      digitalWrite(PIN_LED_ALERTA, alertaLed ? HIGH : LOW);
+    }
+  }
+  else
+  {
+    alertaLed = false;
+    digitalWrite(PIN_LED_ALERTA, LOW);
+  }
+}
+
+void Debounce()
+{
+  int leitura = digitalRead(PIN_BTN);
+  long tempoAtual = millis();
+
+  if (leitura != ultimaLeituraBtn)
+  {
+    ultimoDebounce = tempoAtual;
+  }
+
+  if ((tempoAtual - ultimaLeituraBtn) >= DEBOUNCE)
+  {
+    if (leitura != ultimaEstadoBtn)
+    {
+      ultimaEstadoBtn = leitura;
+
+      if (leitura == LOW)
+      {
+        eventoBtn = true;
+      }
+    }
+  }
+  ultimaLeituraBtn = leitura;
+}
+
+void loop()
+{
+  Debounce();
+
+  Alerta();
+
+  // Parado
+  if (ehSensorAberto() && ehSensorFechado())
+  {
+    PararMotor();
+    estado = EST_PARADO;
+    direcao = DIR_NENHUM;
+  }
+  // Totalmente aberto
+  else if (ehSensorAberto())
+  {
+    PararMotor();
+    estado = EST_PARADO;
+    direcao = DIR_ABRINDO;
+  }
+  // Totalmente fechado
+  else if (ehSensorFechado())
+  {
+    PararMotor();
+    estado = EST_PARADO;
+    direcao = DIR_FECHANDO;
+  }
+  // TIMEOUT
+  if ((estado == EST_ABRINDO || estado == EST_FECHANDO) && inicioMovimento != 0)
+  {
+    if (millis() - inicioMovimento >= TIMEOUT)
+    {
+      Serial.println("Tempo excedido!");
+      if (estado == EST_ABRINDO)
+      {
+        direcao = DIR_ABRINDO;
+      }
+      else if (estado == EST_FECHANDO)
+      {
+        direcao = DIR_FECHANDO;
+      }
+      PararMotor();
+      estado = EST_INTERROMPIDO;
+    }
+  }
+
+  if (eventoBtn)
+  {
+    eventoBtn = false;
+
+    switch (estado)
+    {
+    case EST_PARADO:
+      if (direcao == DIR_ABRINDO)
+      {
+        estado = EST_FECHANDO;
+        FecharPortao();
+      }
+      else if (direcao == DIR_FECHANDO)
+      {
+        estado = EST_ABRINDO;
+        AbrirPortao();
+      }
+      else
+      {
+        estado = EST_ABRINDO;
+        AbrirPortao();
+      }
+      break;
+
+    case EST_ABRINDO:
+      PararMotor();
+      direcao = DIR_ABRINDO;
+      estado = EST_INTERROMPIDO;
+      break;
+
+    case EST_FECHANDO:
+      PararMotor();
+      direcao = DIR_FECHANDO;
+      estado = EST_INTERROMPIDO;
+      break;
+
+    case EST_INTERROMPIDO:
+      if (direcao == DIR_ABRINDO)
+      {
+        estado = EST_FECHANDO;
+        FecharPortao();
+      }
+      else if (direcao == DIR_FECHANDO)
+      {
+        estado = EST_ABRINDO;
+        AbrirPortao();
+      }
+      else
+      {
+        estado = EST_ABRINDO;
+        AbrirPortao();
+      }
+      break;
+    }
+  }
+  delay(5);
 }
